02a6caf40d19464f4d2b4c74c7cb44d0245aa5b6
xuliangzhan <xlz26296x@163.com>
2024-05-29 12:29:07 +0800

    重构组件

--- packages/index.ts
+++ packages/index.ts
@@ -1,11 +1,4 @@
-import VxeUI, { setI18n, setLanguage, setTheme } from './src/core'
-import zhCN from './language/zh-CN'
-
-// 默认中文
-const defaultLanguage = 'zh-CN'
-setI18n(defaultLanguage, zhCN)
-setLanguage(defaultLanguage)
-setTheme('light')
+import VxeUI from './src/core'
 
 export * from './src/core'
 export default VxeUI
