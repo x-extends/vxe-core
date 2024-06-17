d7ee6b89b2fb032272601a236145a54bee02e19d
xuliangzhan <xlz26296x@163.com>
2024-06-17 13:23:32 +0800

    releases 1.0.3

--- types/core/components.d.ts
+++ types/core/components.d.ts
@@ -1,7 +1,7 @@
 import { ComponentOptions } from 'vue'
 
 export type VxeGlobalComponentMethod = (comp: ComponentOptions) => void
-export type VxeGlobalGetComponentMethod = (name: string) => ComponentOptions | null
+export type VxeGlobalGetComponentMethod = <T = ComponentOptions>(name: string) => T | null
 
 export interface VxeGlobalComponents {
   [key: string]: ComponentOptions
