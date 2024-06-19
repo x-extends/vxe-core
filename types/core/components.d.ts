20602cc86acd55c1b7a64341358ab5349c4e1871
xuliangzhan <xlz26296x@163.com>
2024-06-19 18:25:17 +0800

    fix

--- types/core/components.d.ts
+++ types/core/components.d.ts
@@ -1,4 +1,4 @@
 export interface VxeGlobalComponents {}
 
 export type VxeGlobalComponentMethod = (comp: any) => void
-export type VxeGlobalGetComponentMethod = <T = any>(name: keyof VxeGlobalComponents) => T | null
+export type VxeGlobalGetComponentMethod = <T = any>(name: keyof VxeGlobalComponents) => T
