688c0e4fcda63a14b0c49b882f20ad238010777e
xuliangzhan <xlz26296x@163.com>
2024-10-17 19:38:46 +0800

    releases 4.0.16

--- packages/src/i18nStore.ts
+++ packages/src/i18nStore.ts
@@ -1,11 +1,11 @@
-import Vue from 'vue'
+import { reactive } from 'vue'
 
 import { VxeGlobalI18nLocale } from '../../types'
 
 export const i18nConfigStore: {
   language: VxeGlobalI18nLocale,
   langMaps: Partial<Record<VxeGlobalI18nLocale, any>>
-} = Vue.reactive({
+} = reactive({
   language: '',
   langMaps: {}
 })
