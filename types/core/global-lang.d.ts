2dac85cd8a1b71519b1255aec8f02f0dfb4cc5c6
xuliangzhan <xlz26296x@163.com>
2024-11-30 21:10:02 +0800

    releases 3.0.15

--- types/core/global-lang.d.ts
+++ types/core/global-lang.d.ts
@@ -1,15 +1,22 @@
 export type VxeGlobalI18nLocale = '' |
-'zh-CN' |
-'zh-CHT' |
-'zh-TC' |
-'zh-HK' |
-'zh-MO' |
-'zh-TW' |
-'en-US' |
-'ja-JP' |
-'es-ES' |
-'pt-BR' |
-'vi-VN' |
-'ru-RU' |
-'ko-KR' |
-'hu-HU'
+'zh-CN' | // 中文(简体)
+'zh-CHT' | // 中文(繁体)
+'zh-HK' | // 中文(香港)
+'zh-MO' | // 中文(澳门)
+'zh-TW' | // 中文(繁体)
+'zh-TC' | // !!!已废弃!!!
+'en-US' | // 英语(美国)
+'ja-JP' | // 日语
+'es-ES' | // 西班牙语(国际)
+'pt-BR' | // 葡萄牙语
+'vi-VN' | // 越南语
+'ru-RU' | // 俄语
+'ko-KR' | // 朝鲜语
+'hu-HU' | // 匈牙利语
+'ug-CN' | // 维吾尔语
+'uz-UZ' | // 乌兹别克语(西里尔文)
+'nb-NO' | // 挪威语(伯克梅尔)(挪威)
+'hy-AM' | // 亚美尼亚语
+'fr-FR' | // 法语(法国)
+'de-DE' | // 德语(德国)
+'ar-EG' // 阿拉伯语(埃及)
