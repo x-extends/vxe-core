8f819ba5c6bd444d1d9e1b9c134e27bee1ca9de8
xuliangzhan <xlz26296x@163.com>
2024-12-01 18:43:18 +0800

    releases 4.0.19

--- packages/src/i18n.ts
+++ packages/src/i18n.ts
@@ -4,11 +4,19 @@ import { globalConfigStore } from './configStore'
 
 import { VxeGlobalI18nLocale } from '../../types'
 
+let checkInstall = false
+
 export function getI18n (key: string, args?: any) {
   const { langMaps, language } = i18nConfigStore
   const { i18n } = globalConfigStore
   if (i18n) {
     return `${i18n(key, args) || ''}`
+  }
+  if (!checkInstall) {
+    if (!langMaps[language]) {
+      console.error('[vxe] Language not installed. https://vxeui.com/#/start/i18n')
+    }
+    checkInstall = true
   }
   return XEUtils.toFormatString(XEUtils.get(langMaps[language], key, key), args)
 }
